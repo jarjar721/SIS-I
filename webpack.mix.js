b2360dd0b2021b2b6a0e051f78fd2a481239cc0c
FuryAdcom <fastidiosowindows@hotmail.com>
2019-05-24 17:59:55 -0400

    Version 1.1
    Gentelella

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,24 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.copy('vendor/bootstrap/dist/css', 'public/bootstrap/css');
+mix.copy('vendor/bootstrap/dist/js', 'public/bootstrap/js');
+mix.copy('vendor/font-awesome/css', 'public/font-awesome/css');
+mix.copy('vendor/nprogress', 'public/nprogress');
+mix.copy('vendor/iCheck', 'public/iCheck');
+mix.copy('vendor/bootstrap-progressbar', 'public/bootstrap-progressbar');
+mix.copy('vendor/jqvmap', 'public/jqvmap');
+mix.copy('vendor/bootstrap-daterangepicker', 'public/bootstrap-daterangepicker');
+mix.copy('vendor/jquery', 'public/jquery');
+mix.copy('vendor/fastclick', 'public/fastclick');
+mix.copy('vendor/Chart.js', 'public/Chart.js');
+mix.copy('vendor/gauge.js', 'public/gauge.js');
+mix.copy('vendor/skycons', 'public/skycons');
+mix.copy('vendor/Flot', 'public/Flot');
+mix.copy('vendor/flot.orderbars', 'public/flot.orderbars');
+mix.copy('vendor/flot-spline', 'public/flot-spline');
+mix.copy('vendor/flot.curvedlines', 'public/flot.curvedlines');
+mix.copy('vendor/DateJS', 'public/DateJS');
+mix.copy('vendor/jqvmap', 'public/jqvmap');
+mix.copy('vendor/moment', 'public/moment');
